0d3504285f67e86d8d294b685c18e5b88044eb9c
Rui <Rui914@users.noreply.github.com>
2015-07-11 00:44:07 +0900

    Update spawn.lua

--- spawn.lua
+++ spawn.lua
@@ -1,43 +1,32 @@
-creeper.spawning_creeper = {}
-function creeper:register_spawn(name,nodes,max_light,min_light,chance,active_object_count,max_height,spawn_func)
-	creeper.spawning_creeper[name] = true
-	minetest.register_abm({
-		nodenames = nodes,
-		neighbors = {"air"},
-		interval = 30,
-		chance = chance,
-		action = function(pos, node, _, active_object_count_wider)
-			if active_object_count_wider > active_object_count then
-				return
-			end
-			if not creeper.spawning_creeper[name] then
-				return
-			end
-			pos.y = pos.y+1
-			if not minetest.env:get_node_light(pos) then
-				return
-			end
-			if minetest.env:get_node_light(pos) > max_light then
-				return
-			end
-			if minetest.env:get_node_light(pos) < min_light then
-				return
-			end
-			if pos.y > max_height then
-				return
-			end
-			if minetest.env:get_node(pos).name ~= "air" then
-				return
-			end
-			pos.y = pos.y+1
-			if minetest.env:get_node(pos).name ~= "air" then
-				return
-			end
-			if spawn_func and not spawn_func(pos, node) then
-				return
-			end
-			minetest.env:add_entity(pos, name)
+minetest.register_abm({
+	nodenames = {"default:dirt_with_grass"},
+	neighbors = {"air"},
+	interval = 30,
+	chance = 9000,
+	action = function(pos, node, _, active_object_count_wider)
+		if active_object_count_wider > 5 then
+			return
 		end
-	})
-end
-creeper:register_spawn("creeper:creeper",{"default:dirt_with_grass"},5,-1,9000,5,31000)
+		pos.y = pos.y+1
+		if not minetest.get_node_light(pos) then
+			return
+		end
+		if minetest.get_node_light(pos) > 5 then
+			return
+		end
+		if minetest.get_node_light(pos) < -1 then
+			return
+		end
+		if pos.y > 31000 then
+			return
+		end
+		if minetest.get_node(pos).name ~= "air" then
+			return
+		end
+		pos.y = pos.y+1
+		if minetest.get_node(pos).name ~= "air" then
+			return
+		end
+		minetest.add_entity(pos,"creeper:creeper")
+	end
+})
